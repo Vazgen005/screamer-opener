9cf8faee143a532bce6d4e9d79c00626a739b40d
Vazgen005 <70487205+Vazgen005@users.noreply.github.com>
2021-08-26 22:03:51 +0300

    Update scream.py

--- scream.py
+++ scream.py
@@ -1,10 +1,9 @@
 import requests
 import os
-
+import winreg
 
 def get_download_path():
     if os.name == 'nt':
-        import winreg
         sub_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
         downloads_guid = '{374DE290-123F-4565-9164-39C4925E467B}'
         with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
